2847a7105925de8fba8f435833ab685145862b88
Kodamas <superjason95@gmail.com>
2022-06-07 10:53:01 +0200

    feat: Config exemple

--- api/common/Config.exemple.php
+++ api/common/Config.exemple.php
@@ -4,5 +4,5 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
 
-define('TOKEN_SECRET', 'sB7mNbX1vGy3Ej');
+define('TOKEN_SECRET', 'SECRET');
 define('TOKEN_EXPIRE', 60 * 60 * 24);
