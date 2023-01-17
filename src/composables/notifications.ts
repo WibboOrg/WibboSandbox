11af8211ef009983bdf6b02e16e11c3e0971ce05
Kodamas <superjason95@gmail.com>
2023-01-17 09:10:30 +0100

    feat: ShowError and showSuccess

--- src/composables/notifications.ts
+++ src/composables/notifications.ts
@@ -4,3 +4,13 @@ const notifications = ref<string[]>([])
 export const useNotification = () => {
     return { notifications, isError }
 }
+
+export const showError = (message: string) => {
+    isError.value = true
+    notifications.value.push(message)
+}
+
+export const showSuccess = (message: string) => {
+    isError.value = false
+    notifications.value.push(message)
+}
