7f7fbf43eb3e812d661ac1d0f5ad022d9a4a9240
Kodamas <superjason95@gmail.com>
2023-01-17 09:09:31 +0100

    fix: baseSelect

--- src/components/base/BaseSelect.vue
+++ src/components/base/BaseSelect.vue
@@ -1,19 +1,21 @@
 <template>
-    <select
-        class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none"
-        :value="value"
-        @input="$emit('input', ($event.target as HTMLInputElement).value)"
-    >
+    <select class="w-full px-3 py-2 leading-tight text-gray-700 border rounded shadow appearance-none focus:outline-none" v-model="message">
         <option value="" disabled v-if="defaultChoose != ''">{{ defaultChoose }}</option>
         <option v-for="(text, valueOption, index) in options" :key="index" :value="valueOption">{{ text }}</option>
     </select>
 </template>
 
 <script lang="ts" setup>
-defineProps({
-    value: { type: String, default: '' },
+const emit = defineEmits(['update:modelValue'])
+
+const props = defineProps({
+    modelValue: { type: String, default: '' },
     defaultChoose: { type: String, default: '' },
     options: { type: Object, default: null },
 })
-//Record<string|number, string|number>
+
+const message = computed({
+    get: () => props.modelValue,
+    set: (value) => emit('update:modelValue', value),
+})
 </script>
