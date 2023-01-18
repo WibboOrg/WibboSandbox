ce288a196b9cd5ed966bb2aeb16bfb7010680b00
Kodamas <superjason95@gmail.com>
2023-01-18 13:05:35 +0100

    fix: api base dir

---
+++ api/base/BaseController.php
@@ -0,0 +1,69 @@
+<?php
+class BaseController
+{
+    private function requireData(?array $data, array $keyList)
+    {
+        if(!$data) throw new Exception('A field is missing', 400);
+
+        foreach ($keyList as $key)
+        {
+            if(!array_key_exists($key, $data)) {
+                throw new Exception('A field is missing', 400);
+            }
+        }
+    }
+
+    public function getData(array $data): array
+    {
+        $input = json_decode(file_get_contents('php://input'), true);
+        
+        $this->requireData($input, $data);
+
+        return $input;
+    }
+
+    public function getAuth(): object
+    {
+        $headers = apache_request_headers();
+        $authorization = $headers['Authorization'] ?? null;
+        if(!$authorization) throw new HttpException("Unauthorized", 401);
+
+        $token = explode(' ', $authorization)[1];
+
+        return JWT::decode($token);
+    }
+
+    public function getAuthUser(): array
+    {
+        $user = UserDto::getOne($this->getAuth()->id);
+
+        if(!$user) throw new HttpException("Utilisateur non trouver", 404);
+
+        return $user;
+    }
+
+    public function get() 
+    {
+        throw new HttpException("Method get not found", 404);
+    }
+
+    public function post() 
+    {
+        throw new HttpException("Method post not found", 404);
+    }
+
+    public function put() 
+    {
+        throw new HttpException("Method put not found", 404);
+    }
+
+    public function patch() 
+    {
+        throw new HttpException("Method patch not found", 404);
+    }
+
+    public function delete() 
+    {
+        throw new HttpException("Method delete not found", 404);
+    }
+}
