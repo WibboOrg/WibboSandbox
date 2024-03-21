ec9d44f2bc3721a33baeceeeeb8f409a95764d5b
Kodamas <superjason95@gmail.com>
2024-03-21 22:04:58 +0100

    fix: format

--- src/server/utils/upload-api.ts
+++ src/server/utils/upload-api.ts
@@ -1,67 +1,65 @@
 export const uploadApi = async (type: UploadApiType, uploadDatas: UploadApiData[]) => {
-    const { urlAssets, urlCdn } = useRuntimeConfig().public
-    const { uploadUrl } = useRuntimeConfig()
+  const { urlAssets, urlCdn } = useRuntimeConfig().public
+  const { uploadUrl } = useRuntimeConfig()
 
-    try {
-        const payload = new URLSearchParams()
+  try {
+    const payload = new URLSearchParams()
 
-        uploadDatas.forEach((uploadData, index) => {
-            payload.append(`${index}[action]`, uploadData.action)
-            payload.append(`${index}[path]`, uploadData.path)
-            if (uploadData.data) {
-                payload.append(`${index}[data]`, uploadData.data)
-            }
-        })
+    uploadDatas.forEach((uploadData, index) => {
+      payload.append(`${index}[action]`, uploadData.action)
+      payload.append(`${index}[path]`, uploadData.path)
+      if (uploadData.data) {
+        payload.append(`${index}[data]`, uploadData.data)
+      }
+    })
 
-        const response = await fetch(
-            (type === 'assets' ? urlAssets : urlCdn) + uploadUrl,
-            {
-                body: payload,
-                method: 'post',
-                headers: { 'user-agent': 'Mozilla/5.0 (compatible; Wibbo/1.0; +https://wibbo.org/)' }
-            })
+    const response = await fetch((type === 'assets' ? urlAssets : urlCdn) + uploadUrl, {
+      body: payload,
+      method: 'post',
+      headers: { 'user-agent': 'Mozilla/5.0 (compatible; Wibbo/1.0; +https://wibbo.org/)' }
+    })
 
-        const result = await response.text()
+    const result = await response.text()
 
-        if (!result || result !== 'ok') {
-            return false
-        }
-    } catch (e: unknown) {
-        return false
+    if (!result || result !== 'ok') {
+      return false
     }
+  } catch (e: unknown) {
+    return false
+  }
 
-    return true
+  return true
 }
 
 type UploadApiType = 'assets' | 'cdn'
 
 export type UploadApiData = {
-    action: string;
-    path: string;
-    data?: string;
+  action: string
+  path: string
+  data?: string
 }
 
 export const uploadCategoryPath: Record<CategoryKey, CategoryValue> = {
-    "banner": { path: 'images/banner', categoryType: "assets", ext: 'png' },
-    "backgrounds": { path: 'c_images/backgrounds', categoryType: "assets", ext: 'png' },
-    "badge": { path: 'c_images/album1584', categoryType: "assets", ext: 'gif' },
-    "catalogue": { path: 'c_images/catalogue', categoryType: "assets", ext: 'png' },
-    "navigator": { path: 'c_images/navigator', categoryType: "assets", ext: 'png' },
-    "reception": { path: 'c_images/reception', categoryType: "assets", ext: 'png' },
-    "web_promo_small": { path: 'c_images/web_promo_small', categoryType: "assets", ext: 'png' },
-    "notifications": { path: 'c_images/notifications', categoryType: "assets", ext: 'png' },
-    "wibbopages": { path: 'c_images/wibbopages', categoryType: "assets", ext: '' },
-    "icons": { path: 'icons', categoryType: "assets", ext: 'png' },
-    "mp3": { path: 'mp3', categoryType: "assets", ext: 'mp3' },
-    "thumbnail": { path: 'thumbnails', categoryType: "assets", ext: 'png' },
-    "effect": { path: 'bundled/effect', categoryType: "assets", ext: 'nitro' },
-    "figure": { path: 'bundled/figure', categoryType: "assets", ext: 'nitro' },
-    "furniture": { path: 'bundled/furniture', categoryType: "assets", ext: 'nitro' },
-    "generic": { path: 'bundled/generic', categoryType: "assets", ext: 'nitro' },
-    "pet": { path: 'bundled/pet', categoryType: "assets", ext: 'nitro' },
+  banner: { path: 'images/banner', categoryType: 'assets', ext: 'png' },
+  backgrounds: { path: 'c_images/backgrounds', categoryType: 'assets', ext: 'png' },
+  badge: { path: 'c_images/album1584', categoryType: 'assets', ext: 'gif' },
+  catalogue: { path: 'c_images/catalogue', categoryType: 'assets', ext: 'png' },
+  navigator: { path: 'c_images/navigator', categoryType: 'assets', ext: 'png' },
+  reception: { path: 'c_images/reception', categoryType: 'assets', ext: 'png' },
+  web_promo_small: { path: 'c_images/web_promo_small', categoryType: 'assets', ext: 'png' },
+  notifications: { path: 'c_images/notifications', categoryType: 'assets', ext: 'png' },
+  wibbopages: { path: 'c_images/wibbopages', categoryType: 'assets', ext: '' },
+  icons: { path: 'icons', categoryType: 'assets', ext: 'png' },
+  mp3: { path: 'mp3', categoryType: 'assets', ext: 'mp3' },
+  thumbnail: { path: 'thumbnails', categoryType: 'assets', ext: 'png' },
+  effect: { path: 'bundled/effect', categoryType: 'assets', ext: 'nitro' },
+  figure: { path: 'bundled/figure', categoryType: 'assets', ext: 'nitro' },
+  furniture: { path: 'bundled/furniture', categoryType: 'assets', ext: 'nitro' },
+  generic: { path: 'bundled/generic', categoryType: 'assets', ext: 'nitro' },
+  pet: { path: 'bundled/pet', categoryType: 'assets', ext: 'nitro' },
 
-    "article": { path: 'web-promo', categoryType: "cdn", ext: 'png' },
-    "furni": { path: 'furni', categoryType: "cdn", ext: 'png' },
-    "upload": { path: 'uploads', categoryType: "cdn", ext: 'png' },
-    "sound": { path: 'sounds', categoryType: "cdn", ext: 'mp3' },
-};
+  article: { path: 'web-promo', categoryType: 'cdn', ext: 'png' },
+  furni: { path: 'furni', categoryType: 'cdn', ext: 'png' },
+  upload: { path: 'uploads', categoryType: 'cdn', ext: 'png' },
+  sound: { path: 'sounds', categoryType: 'cdn', ext: 'mp3' }
+}
