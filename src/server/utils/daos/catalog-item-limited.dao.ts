7e64a2a05abfa219e571b5ebd647100ef0a79f84
Kodamas <superjason95@gmail.com>
2024-10-11 23:54:24 +0200

    fix: typo

--- src/server/utils/daos/catalog-item-limited.dao.ts
+++ src/server/utils/daos/catalog-item-limited.dao.ts
@@ -1,7 +1,7 @@
 import { Prisma } from "@wibbo/prisma"
 
 export const catalogItemLimitedDao = {
-  getAll:async () => prisma.catalogItemLimited.findMany(),
+  getAll: async () => prisma.catalogItemLimited.findMany(),
   getOne: async (id: number) => prisma.catalogItemLimited.findFirst({ where: { catalogItemId: id } }),
   remove: async (id: number) => prisma.catalogItemLimited.delete({ where: { catalogItemId: id } }),
   removeAll: async (ids: number[]) => prisma.catalogItemLimited.deleteMany({ where: { catalogItemId: { in: ids } } }),
