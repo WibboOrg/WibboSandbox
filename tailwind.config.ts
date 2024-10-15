8bef3e9d1251e6a1dbd8d14dd2d778ad6ebc1f75
Kodamas <superjason95@gmail.com>
2024-10-15 10:08:24 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,28 +2,20 @@ import type { Config } from 'tailwindcss'
 import daisyui from 'daisyui'
 
 export default {
-    mode: 'jit',
-    content: [
-      "./src/components/**/*.{js,vue,ts}",
-      "./src/layouts/**/*.vue",
-      "./src/pages/**/*.vue",
-      "./src/app.vue",
-      "./src/error.vue",
-      "./src/plugins/**/*.{js,ts}",
-      "./nuxt.config.{js,ts}",
-    ],
-    darkMode: 'class',
-      theme: {
-          container: {
-            center: true
-          },
-          screens: {
-            'sm': '576px',
-            'md': '768px',
-            'lg': '992px',
-            'xl': '1200px',
-            '2xl': '1400px',
-          }
-        },
-        plugins: [daisyui],
+  mode: 'jit',
+  content: ['./src/components/**/*.{js,vue,ts}', './src/layouts/**/*.vue', './src/pages/**/*.vue', './src/app.vue', './src/error.vue', './src/plugins/**/*.{js,ts}', './nuxt.config.{js,ts}'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      center: true
+    },
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+      '2xl': '1400px'
+    }
+  },
+  plugins: [daisyui]
 } satisfies Config
